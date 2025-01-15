d9a788b5155d3b0903b719e8aaa0d462c1d65c30
misha-hb <mishahumayun2@gmail.com>
2025-01-15 10:49:07 -0500

    checking sf sample

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 443;
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const port = 443;
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 
-// Define a GET route for the root URL
-app.get('/', (req, res) => {
-    res.send('Welcome to the Weekday Checker API. Use POST /execute to check if today is a weekday.');
-  });
+// // Define a GET route for the root URL
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the Weekday Checker API. Use POST /execute to check if today is a weekday.');
+//   });
 
-app.post('/execute', (req, res) => {
-    const today = new Date();
-    const day = today.getDay();
-    const isWeekday = day >= 1 && day <= 5;
-    res.setHeader('Content-Type', 'application/json');
+// app.post('/execute', (req, res) => {
+//     const today = new Date();
+//     const day = today.getDay();
+//     const isWeekday = day >= 1 && day <= 5;
+//     res.setHeader('Content-Type', 'application/json');
 
-    res.json({
-        sendEmail: isWeekday
-    });
-});
+//     res.json({
+//         sendEmail: isWeekday
+//     });
+// });
 
-app.listen(port, () => {
-  console.log(`Custom Activity app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Custom Activity app listening on port ${port}`);
+// });
