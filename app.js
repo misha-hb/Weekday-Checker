857906f5ca3e73df65e04ea96a6e420e62ba1e97
misha-hb <mishahumayun2@gmail.com>
2025-05-26 12:56:43 -0400

    using cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'public')));
  app.use((req, res, next) => {
