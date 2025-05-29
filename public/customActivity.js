88613627fafbc40da02a9d4041c79f610b7ea20f
misha-hb <mishahumayun2@gmail.com>
2025-05-29 16:11:28 -0400

    fixed time format

--- public/customActivity.js
+++ public/customActivity.js
@@ -51,7 +51,7 @@ define(["postmonger"], function (Postmonger) {
         });
 
         if (selectedTime) {
-            const timeParts = selectedTime.match(/(\d{2}):(\d{2})\s?(AM|PM)/i);
+            const timeParts = selectedTime.match(/(\d{1,2}):(\d{2})\s?(AM|PM)/i);
             if (timeParts) {
                 const [, hour, minute, ampm] = timeParts;
                 $('#hour').val(hour);
@@ -64,35 +64,6 @@ define(["postmonger"], function (Postmonger) {
 
     }
     
-    /*async function getAccessToken() {
-        const tokenUrl = `https://mcjtfjy4vdxv2ww8bm6y0zn1rpf4.auth.marketingcloudapis.com/v2/token`;  // Replace with your subdomain
-        const headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-
-        const body = JSON.stringify({
-            grant_type: "client_credentials",
-            client_id: "cvv69pjvucdwkhhpjqbam0gh",  // Replace with your Client ID
-            client_secret: "GMIZ3FdMYaDnew4T1tCKFxf9"  // Replace with your Client Secret
-        });
-
-        try {
-            const response = await fetch(tokenUrl, {
-                method: 'POST',
-                headers: headers,
-                body: body
-            });
-
-            if (response.ok) {
-                const data = await response.json();
-                accessToken = data.access_token;  // Store the access token
-                console.log("Access token:", accessToken);
-            } else {
-                console.error("Error fetching access token:", response.status, response.statusText);
-            }
-        } catch (error) {
-            console.error("Error fetching access token:", error);
-        }
-    }*/
 
     function save() {
 
